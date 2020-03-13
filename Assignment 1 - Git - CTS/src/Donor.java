e3488f295e7a934b7df1ba07454d60f24daf322f
deliagrigore <deliagrigore@localhost>
2020-03-14 01:06:05 +0200

    Adding the Donor class

---
+++ Assignment 1 - Git - CTS/src/Donor.java
@@ -0,0 +1,19 @@
+
+public class Donor {
+	
+	private String name;
+	private int age;
+	private String blodType;
+	private int kg;
+	
+	
+	public Donor(String name, int age, String blodType, int kg) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.blodType = blodType;
+		this.kg = kg;
+	}
+	
+	
+}
